be13f07d26ec9329ceaf2f8f7e70af8aab2e1a5c
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-05-10 17:12:42 +0400

    Initial Commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
     # Local Apps
     'shop',
+    'cart',
 
 ]
 
@@ -110,3 +111,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cart Session
+CART_SESSION_ID = 'cart'

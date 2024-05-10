b998d0a05f29cbc7eb76777c684c967109adad26
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-05-10 17:47:24 +0400

    add urls for views

---
+++ cart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apps import CartConfig
+from . import views
+
+app_name = CartConfig.name
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.card_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+]

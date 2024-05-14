58c64e176160b8cde0f030f7df11c48fe06ed4b5
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-05-14 12:44:09 +0400

    Add redirecting func and also order item for stripe checkout session

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
+
 import stripe
 from django.conf import settings
-from django.shortcuts import get_object_or_404, reverse, redirect
+from django.shortcuts import get_object_or_404, reverse, redirect, render
 
 from orders.models import Order
 
@@ -25,6 +27,18 @@ def payment_process(request):
             'cancel_url': cancel_url,
             'line_items': []
         }
+        # add order items to the Stripe checkout session
+        for item in order.items.all():
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.product.name,
+                    },
+                },
+                'quantity': item.quantity,
+            })
         # create Stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
 
@@ -32,3 +46,11 @@ def payment_process(request):
         return redirect(session.url, code=303)
     else:
         return redirect(request, 'payment/process.html', locals())
+
+
+def payment_completed(request):
+    return render(request, 'payment/completed.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payments/canceled.html')

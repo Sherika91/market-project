ef9388f95b1c94342e0ecd59c8587f96c81c35c7
Sherika91 <109757389+Sherika91@users.noreply.github.com>
2024-05-14 12:29:57 +0400

    Add payemnt

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
@@ -20,9 +21,11 @@ def order_create(request):
             cart.clear()
             # launch an asynchronous task
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
+
     else:
         form = OrderCreateForm()
     return render(request,
